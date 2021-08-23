8887968586f2378ea2e7dd5fd4a9796e57832add
Radek <rad.bialkowski@gmail.com>
2021-08-23 12:44:21 +0200

    modyfikacja app.module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
+import { UserComponent } from './create-user/user.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {ProductsComponent} from "./products/products.component";
+import {CategoryComponent} from "./category/category.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     ProductsComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
