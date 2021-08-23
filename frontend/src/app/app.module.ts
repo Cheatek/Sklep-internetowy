afba3dc944c6c453bf50d6a8ea2e962ea488efae
Artur Dudek <arturdudek97@gmail.com>
2021-08-23 12:25:04 +0200

    Dodano obsługe wypisywania wszystkich uzytkowników

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
+import { UserComponent } from './create-user/user.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { ListAllUsersComponent } from './list-all-users/list-all-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    ListAllUsersComponent
   ],
   imports: [
     BrowserModule,
