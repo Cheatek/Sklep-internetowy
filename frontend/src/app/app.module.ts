6858d5dcf7c0999daa385dbee03737301b55326f
AgaMac11 <agatamaciesza@gmail.com>
2021-08-23 12:17:18 +0200

    ProductComponent

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import {ProductsComponent} from "./products/products.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
