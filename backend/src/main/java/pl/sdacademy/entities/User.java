a4e0735cfd69b7b466bf56e9b9eb0b389d68edcd
Artur Dudek <arturdudek97@gmail.com>
2021-08-24 14:48:50 +0200

    Dodano walidacje Usera

--- backend/src/main/java/pl/sdacademy/entities/User.java
+++ backend/src/main/java/pl/sdacademy/entities/User.java
@@ -2,15 +2,31 @@ package pl.sdacademy.entities;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Entity
 public class User {
+    private static final String emailChecker = "^(?:[a-z0-9!#$%&'*+|=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+|=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])$";
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Email(regexp = emailChecker, message = "Niepoprawny email")
+    @NotEmpty(message = "Email nie może być pusty")
     @Column(unique = true)
     private String email; //login
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{6,}$", message = "Hasło powinno posiadać przynajmniej 6 znaków, 1 małą litere, 1 dużą litere, 1 cyfre, oraz 1 znak specjalny")
+    @NotEmpty(message = "Hasło nie może być puste")
     private String password;
+/*^                 # start-of-string
+            (?=.*[0-9])       # a digit must occur at least once
+            (?=.*[a-z])       # a lower case letter must occur at least once
+            (?=.*[A-Z])       # an upper case letter must occur at least once
+            (?=.*[@#$%^&+=])  # a special character must occur at least once
+            (?=\S+$)          # no whitespace allowed in the entire string
+            .{6,}             # anything, at least eight places though
+    $                 # end-of-string*/
     @Embedded
     private Address address;
 
