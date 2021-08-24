341d068eadda92018b945701cb43aabc58b88d50
Artur Dudek <arturdudek97@gmail.com>
2021-08-24 09:48:21 +0200

    dodano stylowanie dla tworzenia produktu

--- frontend/src/app/products/products.component.ts
+++ frontend/src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Product} from "./product";
+import {Category} from "../category/category";
 
 
 @Component({
@@ -19,13 +20,18 @@ export class ProductsComponent implements OnInit {
     brand: '',
     size: '',
     category: ''
-  }
+  };
+
+  categories: Category[] = [];
   ngOnInit(): void {
+    this.httpClient
+      .get<Category[]>('http://localhost:8080/getAllCategories')
+      .subscribe(response=> this.categories = response);
   }
   onSubmit(): void {
     this.httpClient
       .post<Product>('http://localhost:8080/addProduct', this.product)
-      .subscribe(response=> console.log(response))
+      .subscribe(response=> console.log(response));
   }
 
 }
