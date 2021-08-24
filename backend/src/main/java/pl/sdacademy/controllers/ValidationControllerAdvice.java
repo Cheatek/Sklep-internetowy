78600e54a55345c99690ae02703b46e585fbad3a
Radek <rad.bialkowski@gmail.com>
2021-08-24 14:06:31 +0200

    validation controller

--- backend/src/main/java/pl/sdacademy/controllers/ValidationControllerAdvice.java
+++ backend/src/main/java/pl/sdacademy/controllers/ValidationControllerAdvice.java
@@ -24,5 +24,6 @@ public class ValidationControllerAdvice {
                         FieldError::getField,
                         Collectors.mapping(FieldError::getDefaultMessage, Collectors.toSet())
                 ));
+
     }
 }
