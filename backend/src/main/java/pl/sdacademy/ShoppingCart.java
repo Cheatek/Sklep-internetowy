dfedc35724240f83e351d1b6ea45a2c698c7d62d
AgaMac11 <agatamaciesza@gmail.com>
2021-08-26 10:58:27 +0200

    add class ShoppingCart

---
+++ backend/src/main/java/pl/sdacademy/ShoppingCart.java
@@ -0,0 +1,9 @@
+package pl.sdacademy;
+
+import org.springframework.stereotype.Component;
+import pl.sdacademy.entities.OrderLine;
+@Component
+public class ShoppingCart {
+
+    private OrderLine orderLine;
+}
